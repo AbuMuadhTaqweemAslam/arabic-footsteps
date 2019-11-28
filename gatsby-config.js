f3d57990e7aa980670203532689de8b0f996cb02
Azmol-Miah <azmol@azmolmiah.com>
2019-11-28 13:40:17 +0000

    added a materials ui gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,6 +134,15 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
